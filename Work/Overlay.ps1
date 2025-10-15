db05fe654be190b109b6e2f72c073e508510db9e
MartyFiles <volodyamartishevskiy@gmail.com>
2025-10-15 22:19:23 +0300

    Рабочие файлы

--- Work/Overlay.ps1
+++ Work/Overlay.ps1
@@ -10,11 +10,11 @@ Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase
         AllowsTransparency="True"
         WindowState="Maximized"
         Topmost="False"
-        ShowInTaskbar="True">
+        ShowInTaskbar="False">
     <Window.Background>
-<LinearGradientBrush StartPoint="0,0" EndPoint="0,1">
-    <GradientStop Color="#800000FF" Offset="1.0"/>  <!-- Синий внизу -->
-    <GradientStop Color="#804B0082" Offset="0.0"/>  <!-- Фиолетовый вверху -->
+        <LinearGradientBrush StartPoint="0,0" EndPoint="0,1">
+            <GradientStop Color="#800000FF" Offset="1.0"/>
+            <GradientStop Color="#804B0082" Offset="0.0"/>
         </LinearGradientBrush>
     </Window.Background>
     <Grid>
@@ -46,7 +46,7 @@ $window = [Windows.Markup.XamlReader]::Load($reader)
 # Находим элемент для основного текста
 $mainText = $window.FindName("MainMessage")
 
-# Функция обновления текста из файла с учётом OEM (CP866)
+# Функция обновления текста из файла
 function Update-Message {
     if (Test-Path $MessageFile) {
         try {
@@ -58,8 +58,6 @@ function Update-Message {
     }
 }
 
-
-
 # Таймер обновления сообщения каждые 300 мс
 $timer = New-Object System.Windows.Threading.DispatcherTimer
 $timer.Interval = [TimeSpan]::FromMilliseconds(300)
@@ -69,18 +67,10 @@ $timer.Start()
 # Начальное обновление
 Update-Message
 
-# Импортируем WinAPI для управления окном
+# Импорт WinAPI
 Add-Type @"
 using System;
 using System.Runtime.InteropServices;
-public class WinAPI {
-    [DllImport("user32.dll")]
-    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-    public const int SWP_NOSIZE = 0x0001;
-    public const int SWP_NOMOVE = 0x0002;
-    public const int SWP_NOACTIVATE = 0x0010;
-    public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
-}
 public class User32 {
     [DllImport("user32.dll")]
     public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
@@ -88,28 +78,18 @@ public class User32 {
     public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
     public const int GWL_EXSTYLE = -20;
     public const int WS_EX_TOOLWINDOW = 0x00000080;
+    public const int WS_EX_TRANSPARENT = 0x00000020;
 }
 "@
 
-# Получаем handle окна
-$hwnd = (New-Object System.Windows.Interop.WindowInteropHelper $window).Handle
+# Установка стиля после полной инициализации окна
+$window.Add_SourceInitialized({
+    $hwnd = (New-Object System.Windows.Interop.WindowInteropHelper $window).Handle
 
-# Добавляем стиль WS_EX_TOOLWINDOW, чтобы не показывать окно в панели задач
-$exStyle = [User32]::GetWindowLong($hwnd, [User32]::GWL_EXSTYLE)
-$newStyle = $exStyle -bor [User32]::WS_EX_TOOLWINDOW
-[User32]::SetWindowLong($hwnd, [User32]::GWL_EXSTYLE, $newStyle) | Out-Null
-
-# Функция удержания окна поверх всех, без активации
-function Set-AlwaysOnTop {
-    [WinAPI]::SetWindowPos($hwnd, [WinAPI]::HWND_TOPMOST, 0,0,0,0,
-        [WinAPI]::SWP_NOMOVE -bor [WinAPI]::SWP_NOSIZE -bor [WinAPI]::SWP_NOACTIVATE) | Out-Null
-}
-
-# Таймер для повторного удержания окна наверху каждые 500 мс
-$topmostTimer = New-Object System.Windows.Threading.DispatcherTimer
-$topmostTimer.Interval = [TimeSpan]::FromMilliseconds(500)
-$topmostTimer.Add_Tick({ Set-AlwaysOnTop })
-$topmostTimer.Start()
+    $exStyle = [User32]::GetWindowLong($hwnd, [User32]::GWL_EXSTYLE)
+    $newStyle = $exStyle -bor [User32]::WS_EX_TOOLWINDOW -bor [User32]::WS_EX_TRANSPARENT
+    [User32]::SetWindowLong($hwnd, [User32]::GWL_EXSTYLE, $newStyle) | Out-Null
+})
 
 # Показываем окно
 $window.ShowDialog() | Out-Null
